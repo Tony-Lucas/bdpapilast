ccca91be000224e3c34bf955c2f1ae0d80f8c98a
Tony <tonylucas1997@hotmail.com>
2021-05-06 13:09:30 -0300

    db access

--- src/config/server.js
+++ src/config/server.js
@@ -14,10 +14,12 @@ const adminRoutes = require("../routes/Admin");
 const mercadoriasRoutes = require("../routes/Mercadoria");
 const notasRoutes = require("../routes/Nota");
 const vendasRoutes = require("../routes/Venda");
+const loginRoute = require("../routes/Login");
 
 app.use("/admin",adminRoutes);
 app.use("/mercadoria",mercadoriasRoutes);
 app.use("/notas",notasRoutes)
 app.use("/vendas",vendasRoutes)
+app.use("/login",loginRoute);
 
 app.listen(process.env.PORT || 3000);
