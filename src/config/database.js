6b9f5d8d28df205b2b24f6ca0837de715c2d1635
Tony Lucas <tonyjogos123@gmail.com>
2021-05-09 18:24:48 -0300

    asdasda

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("bdpapidb","tonyjogos123","55425610a",{
+const sequelize = new Sequelize("bpdapidb","tonyjogos123","55425610a",{
     host:"mysql743.umbler.com",
     dialect:"mysql",
     raw:true,
